afcce39d48aa1f3a5feb7194521e1878db670512
luceverus <luceverus@naver.com>
2020-12-27 14:35:21 +0900

    #2.1

---
+++ app.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x, y);
+}
+
+function onMouseDown(event) {
+    painting = true;
+}
+
+function onMouseUp(event) {
+    stopPainting();
+}
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
